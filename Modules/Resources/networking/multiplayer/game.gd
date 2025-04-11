11e5d004cd31be3eb5fdbe7cf5c8084db7c507e9
bezark <johnbezark@gmail.com>
2025-04-11 17:28:31 -0400

    pre 4_11

--- Modules/Resources/networking/multiplayer/game.gd
+++ Modules/Resources/networking/multiplayer/game.gd
@@ -1,8 +1,10 @@
 extends Node
 
+
 func _ready() -> void:
 	multiplayer.peer_connected.connect(peer_joined)
-	
+	multiplayer.peer_disconnected.connect(remove_player)
+
 
 
 func _on_server_button_down() -> void:
@@ -11,31 +13,45 @@ func _on_server_button_down() -> void:
 	multiplayer.multiplayer_peer = peer
 	load_scene()
 
+
 func _on_client_button_down() -> void:
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client("localhost", 7878)
-	load_scene()
 	multiplayer.multiplayer_peer = peer
+	load_scene()
 
 
 func peer_joined(id):
 	prints(id, "joined!")
-	#print(multiplayer.get_peers())
-	load_player(id)
-	
+	prints(
+		"i am ",
+		multiplayer.get_unique_id(),
+		"peers are ",
+		multiplayer.get_peers(),
+		"server",
+		multiplayer.is_server()
+	)
+	#for peer in multiplayer.get_peers():
+		#load_player(peer)
+
 
 func load_scene():
 	var old_nodes = get_children()
 	for node in old_nodes:
 		node.queue_free()
 	load_player(multiplayer.get_unique_id())
-		
-const PLAYER = preload("res://multiplayer/player.tscn")
+
+
+
 
 func load_player(id):
+	var PLAYER = preload("res://multiplayer/player.tscn")
 	var new_player = PLAYER.instantiate()
-	new_player.id = id
-	add_child(new_player)
-	#prints(id, multiplayer.get_unique_id())
-	#print(id == multiplayer.get_unique_id())
-	#new_player.local_control = id == multiplayer.get_unique_id()
+	new_player.peer_id = id
+	add_child(new_player, true)
+
+
+func remove_player(id: int):
+	if not has_node(str(id)):
+		return
+	get_node(str(id)).queue_free()
--- Modules/Resources/networking/multiplayer/player.gd
+++ Modules/Resources/networking/multiplayer/player.gd
@@ -1,21 +1,32 @@
 extends CharacterBody2D
 
-
 var speed = 200.0
-var id
-var local_control = true
 
-func _physics_process(delta: float) -> void:
-	if local_control:
-		velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-		velocity *= speed
-		prints(self.name, velocity)
-		move_and_slide()
-		
+
+
+
+
+
+@export var peer_id : int : 
+	set(value):
+		peer_id = value
+		name = str(peer_id)
+		set_multiplayer_authority(peer_id)
+
 func _ready():
-	set_multiplayer_authority(id)
-	# Set process functions for current player.
 	var is_local = is_multiplayer_authority()
 	set_process_input(is_local)
 	set_physics_process(is_local)
 	set_process(is_local)
+
+	print("------------")
+	prints(self, get_multiplayer_authority())
+	print("------------")
+
+
+func _physics_process(delta: float) -> void:
+	
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity *= speed
+		
+	move_and_slide()
